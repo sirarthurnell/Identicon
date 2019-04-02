2a430555127621ddf33d9aa8a15d320ba624533a
sirarthurnell <sirarthurnell@gmail.com>
2019-04-03 00:58:39 +0200

    Pick color.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,35 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Generates an identicon.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+  Program's main entry.
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  @doc """
+  Picks the color to use.
+  """
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    [r, g, b]
+  end
+
+  @doc """
+  Gets the bytes of the MD5
+  representation of `input`.
+  """
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
   end
 end
---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule Identicon.Image do
+  @moduledoc """
+  Holds information about the image.
+  """
+  defstruct hex: nil
+end
